53530f7eeb02344bf68d869d9c28eb8712983dc9
Datirsayali12 <sayalidatir70@gmail.com>
2024-05-08 23:09:13 +0530

    add changes

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =bool(env('DEBUG'))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -168,15 +168,15 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AWS_ACCESS_KEY_ID = 'AKIA2UC3AAKLLMSR4A7T '
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'carsearcher'
-# AWS_S3_SIGNATURE_NAME = 's3v4',
-# AWS_S3_REGION_NAME = 'ap-south-1'
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL =  None
-# AWS_S3_VERITY = True
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = 'AKIA2UC3AAKLLMSR4A7T '
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'carsearcher'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'ap-south-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
